3e858be77d393685e76046fe125d84f16b636a16
Mike Garabedian <mike.garabedian@riverbed.com>
2018-10-31 15:33:45 -0400

    Add interfaces and VIFGs to list_sources script

--- examples/list_sources.py
+++ examples/list_sources.py
@@ -44,6 +44,38 @@ class PacketCaptureApp(AppResponseApp):
                       lambda job: job.data.config.vifgs,
                       self.appresponse.capture.get_vifgs)
 
+        # Show Interfaces and VIFGs (skip if MIFG appliance)
+        if ifg.type == 'vifgs':
+            # Show interfaces
+            headers = ['name', 'description', 'status', 'bytes_total',
+                       'packets_dropped', 'packets_total']
+            data = []
+            for iface in self.appresponse.capture.get_interfaces():
+                data.append([
+                    iface.name,
+                    iface.data.config.description,
+                    iface.status,
+                    iface.stats.bytes_total.total,
+                    iface.stats.packets_dropped.total,
+                    iface.stats.packets_total.total,
+                ])
+            self.console('Interfaces', data, headers)
+
+            headers = ['id', 'name', 'enabled', 'filter', 'bytes_received',
+                       'packets_duped', 'packets_received']
+            data = []
+            for vifg in self.appresponse.capture.get_vifgs():
+                data.append([
+                    vifg.data.id,
+                    vifg.data.config.name,
+                    vifg.data.config.enabled,
+                    vifg.data.config.filter,
+                    vifg.data.state.stats.bytes_received.total,
+                    vifg.data.state.stats.packets_duped.total,
+                    vifg.data.state.stats.packets_received.total,
+                ])
+            self.console('VIFGs', data, headers)
+
         # Show capture jobs
         headers = ['id', 'name', ifg.type, 'filter', 'state',
                    'start_time', 'end_time', 'size']
