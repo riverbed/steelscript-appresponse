3c887ec56e8dd7b615cf0f00305f83dbd2232e62
Mircea Zetea <mzetea@riverbed.com>
2019-05-24 16:01:01 +0300

    temp - moved away from wiremock

--- conftest.py
+++ conftest.py
@@ -22,28 +22,28 @@ def user_auth():
 
 @pytest.fixture()
 def app(user_auth):
-    return AppResponse(WIREMOCK_SERVER, auth=user_auth)
-    # return AppResponse(AR_ENDPOINT, auth=user_auth)
+    # return AppResponse(WIREMOCK_SERVER, auth=user_auth)
+    return AppResponse(AR_ENDPOINT, auth=user_auth)
 
 
 def pytest_configure(config):
     steelscript.common.connection.Connection.REST_DEBUG = 2
     steelscript.common.connection.Connection.REST_BODY_LINES = 200
-    start_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_START_RECORDING)
-    reset_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_RESET)
-    reset = requests.delete(reset_api)
-    req = requests.post(start_api, data=json.dumps(
-        {'targetBaseUrl': AR_ENDPOINT}))
-    print("Started WireMock Recording for ARX. Response from server: %s" %
-          req.content)
+    # start_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_START_RECORDING)
+    # reset_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_RESET)
+    # reset = requests.delete(reset_api)
+    # req = requests.post(start_api, data=json.dumps(
+    #     {'targetBaseUrl': AR_ENDPOINT}))
+    # print("Started WireMock Recording for ARX. Response from server: %s" %
+    #       req.content)
 
 
-def pytest_unconfigure(config):
-    stop_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_STOP_RECORDING)
-    snapshot_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_SNAPSHOT)
-    req = requests.post(stop_api, data=json.dumps(
-        {'targetBaseUrl': AR_ENDPOINT}))
-    print("Stopped WireMock Recording for ARX. Response from server: %s" %
-          req.content)
-    snap = requests.post(snapshot_api)
-    print("Created snapshot. Response was: %s" % snap.content)
+# def pytest_unconfigure(config):
+#     stop_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_STOP_RECORDING)
+#     snapshot_api = "%s/%s" % (WIREMOCK_SERVER, WIREMOCK_SNAPSHOT)
+#     req = requests.post(stop_api, data=json.dumps(
+#         {'targetBaseUrl': AR_ENDPOINT}))
+#     print("Stopped WireMock Recording for ARX. Response from server: %s" %
+#           req.content)
+#     snap = requests.post(snapshot_api)
+#     print("Created snapshot. Response was: %s" % snap.content)
